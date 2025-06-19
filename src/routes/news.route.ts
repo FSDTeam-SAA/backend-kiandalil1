d02f58c9e59223691c631b97556089099c8d51e3
zihadulislam2 <zihadul708@gmail.com>
2025-06-19 14:36:19 +0600

    fix: update news route to include missing controller methods for patch and delete

--- src/routes/news.route.ts
+++ src/routes/news.route.ts
@@ -1,14 +1,19 @@
 import express from 'express'
 import { isAdmin, protect } from '../middlewares/auth.middleware'
 import { upload } from '../middlewares/multer.middleware'
-import { createNews, getAllNews } from '../controllers/news.controller'
+import {
+  createNews,
+  getAllNews,
+  deleteNews,
+  updateNews,
+} from '../controllers/news.controller'
 
 
 const router = express.Router()
 
 router.get('/news', getAllNews)
 router.post('/news', protect,isAdmin, upload.single('image'), createNews)
-router.patch('/news/:id', protect, )
-router.delete('/review/:id', )
+router.patch('/news/:id', protect, updateNews)
+router.delete('/review/:id', deleteNews)
 
 export default router
