a489bc0b791854360eae01e167052819f2896bfa
undefinedSahed <sahedrahmanshakil@gmail.com>
2025-06-23 16:01:05 +0600

    get single news added

--- src/routes/news.route.ts
+++ src/routes/news.route.ts
@@ -6,6 +6,7 @@ import {
   getAllNews,
   deleteNews,
   updateNews,
+  getSingleNews,
 } from '../controllers/news.controller'
 
 const router = express.Router()
@@ -13,6 +14,7 @@ const router = express.Router()
 router.get('/news', getAllNews)
 router.post('/news', protect, isAdmin, upload.single('image'), createNews)
 router.patch('/news/:id', protect, isAdmin, upload.array('images'), updateNews)
+router.get('/news/:id', getSingleNews)
 router.delete('/news/:id', protect, isAdmin, deleteNews)
 
 export default router
